efc1e1228c2769b64dc21716633a14569e2ef08b
sandra billard <florian.c@tassesforce.com>
2019-09-19 13:25:00 +0200

    Ajout des forward headers

--- Startup.cs
+++ Startup.cs
@@ -7,6 +7,7 @@ using lug.Middleware.Request;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.HttpOverrides;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.SpaServices.Webpack;
@@ -67,11 +68,16 @@ namespace cairn_vue
             {
                 app.UseExceptionHandler("/Home/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
+                //app.UseHsts();
             }
 
             app.UseMiddleware<SerilogMiddleware>();
             app.UseMiddleware<RequestResponseLoggingMiddleware>();
+
+            app.UseForwardedHeaders(new ForwardedHeadersOptions
+            {
+                ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto
+            });
 
             // app.UseHttpsRedirection();
             app.UseSession();
